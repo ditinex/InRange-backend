7d940ba7be5c6242511117134de2c5364947fdea
Souradeep Das <48976985+Souradeep-16@users.noreply.github.com>
2020-09-27 23:44:22 +0530

    .

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const Config = require('../config.js');
-const { UnauthorizedError } = require('../controllers/baseController')
+const { UnauthorizedError, IsExists } = require('../controllers/baseController')
+const { Admin, Otp, User, Task, Mongoose, Review } = require('../models')
 
 const VerifyToken = (req, res, next) => {
 	try{
@@ -8,6 +9,9 @@ const VerifyToken = (req, res, next) => {
 	        let token = req.headers.authorization.split(" ")[1];
 	        jwt.verify(token, Config.secret, (err, user) => {
 	        	if(err)
+					return UnauthorizedError(res)
+				const isUserExists = await IsExists(User,{_id: user.id, active_session_refresh_token: token})
+				if(!isUserExists)
 					return UnauthorizedError(res)
 				req.user_id = user.id
 				req.mobile = user.mobile
