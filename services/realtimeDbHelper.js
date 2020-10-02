f6ace9ae6f9f48b986612019a7cd623e5758b985
ditinex <asifakramsk@gmail.com>
2020-10-02 20:38:48 +0530

    socket

--- services/realtimeDbHelper.js
+++ services/realtimeDbHelper.js
@@ -3,7 +3,7 @@ const socketIOClient = require('socket.io-client')
 
 const taskChange = socketIOClient('http://localhost:'+Config.port+'/realtime-task');
 taskChange.on('connect', socket=>{ 
-    console.log('Realtime DB Task Table helper Socket Connected.');
+    console.log('Realtime DB Task Table Helper Socket Connected.');
 });
 
 exports.taskChange = taskChange
