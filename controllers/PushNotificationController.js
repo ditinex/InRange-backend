201aa13d6cd7ea757e2075adf6edea47e227d28b
Souradeep Das <48976985+Souradeep-16@users.noreply.github.com>
2020-12-14 11:46:43 +0530

    ..

--- controllers/PushNotificationController.js
+++ controllers/PushNotificationController.js
@@ -47,7 +47,7 @@ const PushTextNotification = async(title,description,tokens,image_url) => {
             }
         };
 
-        await HandleSend(message);
+      await HandleSend(message);
         
     }
     catch (err){
@@ -59,28 +59,35 @@ const PushTextNotification = async(title,description,tokens,image_url) => {
 const PushMessage = async(title,description,tokens,collapseId) => {
 
     try{
-        const payload = {
-            notification: {
-              body: description,
+      const message = {
+        token: tokens[0],
+        notification: {
+          body: description,
+          title: title,
+        },
+        data: {type: 'notification'},
+        android: {
+          notification: {
+            image: image_url,
+            icon: "https://i.ibb.co/MMVSRRR/icon.png",
+          },
+          priority: "high",
+          restrictedPackageName: "com.inrangeit",
+          collapseKey: collapseId
+        },
+        apns: {
+          payload: {
+            aps: {
+              'mutable-content': true,
             },
-            data: {type: 'chat'},
-        };
-        const options = {
-          collapseKey: 'collapseId',
-          // priority: 'high',
-          // restrictedPackageName: "com.inrangeit",
-          // 'mutable-content': true,
-          // contentAvailable: false
-        };
+          },
+          fcm_options: {
+            image: image_url,
+          },
+        }
+    };
 
-        await admin.messaging()
-        .sendToDevice(tokens,payload,options)
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => {
-          console.log(error);
-        });
+      await HandleSend(message);
     }
     catch (err){
         console.log(err)
